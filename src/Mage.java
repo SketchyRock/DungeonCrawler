ede439c6d1bd9bb0e4c7235774ca5fb7e06f8628
Rock <hinsenzo15@gmail.com>
2025-05-20 18:51:09 +0000

    created 3 classes for the game

---
+++ src/Mage.java
@@ -0,0 +1,33 @@
+public class Mage implements CharacterClass{
+    
+    
+    @Override
+    public String getClassName(){return ("Mage");}
+    
+    @Override
+    public int getBaseHP(){return (75);}
+    
+    @Override
+    public int getBaseMP(){return (75);}
+    
+    @Override
+    public int getBaseSP(){return (45);}
+
+    @Override
+    public int getBaseStr(){return (5);}
+
+    @Override
+    public int getBaseDef(){return (5);}
+
+    @Override
+    public int getBaseIntel(){return (16);}
+
+    @Override
+    public int getBaseAg(){return (12);}
+
+    @Override
+    public int getBaseLvl(){return (38);}
+
+    
+
+}
---
+++ src/Rouge.java
@@ -0,0 +1,33 @@
+public class Rouge implements CharacterClass{
+    
+    
+    @Override
+    public String getClassName(){return ("Rouge");}
+    
+    @Override
+    public int getBaseHP(){return (100);}
+    
+    @Override
+    public int getBaseMP(){return (20);}
+    
+    @Override
+    public int getBaseSP(){return (75);}
+
+    @Override
+    public int getBaseStr(){return (6);}
+
+    @Override
+    public int getBaseDef(){return (6);}
+
+    @Override
+    public int getBaseIntel(){return (10);}
+
+    @Override
+    public int getBaseAg(){return (16);}
+
+    @Override
+    public int getBaseLvl(){return (38);}
+
+    
+
+}
